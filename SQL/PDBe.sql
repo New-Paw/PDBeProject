3d72a803c1b32735069fa9d1faf80c57451daea8
gagalittleduck <96385238+gagalittleduck@users.noreply.github.com>
2025-11-28 00:00:31 +0100

    Refactor SQL to replace map with SEntities and add MEntities
    Removed the 'map' table and replaced it with 'SEntities' table operations. Added 'MEntities' table with associated triggers to auto-compute propoties when updating a image.

--- SQL/PDBe.sql
+++ SQL/PDBe.sql
@@ -1,14 +1,32 @@
-DROP TABLE map;
 
-CREATE TABLE map (
-    entity_Id NUMBER PRIMARY KEY,
+DROP TABLE MENTITIES;
+DROP TABLE SEntities;
+
+CREATE TABLE SEntities (
+    SID NUMBER PRIMARY KEY,
     entity_Name VARCHAR2(32),
     entity_Type VARCHAR2(32),
     geometry SDO_GEOMETRY
 );
 
+INSERT INTO SEntities VALUES (
+  0, 'zoo', 'border',
+  SDO_GEOMETRY(
+    2002,
+    NULL,
+    NULL,
+    SDO_ELEM_INFO_ARRAY(1,2,1),
+    SDO_ORDINATE_ARRAY(
+      0,0,
+      500,0,
+      500,500,
+      0,500,
+      0,0
+    )
+  )
+);
 
-INSERT INTO map VALUES(
+INSERT INTO SEntities VALUES(
     1, 'panda_House', 'building',
     SDO_GEOMETRY(
     2003,
@@ -22,7 +40,7 @@ INSERT INTO map VALUES(
     )
 );
 
-INSERT INTO map VALUES (
+INSERT INTO SEntities VALUES (
     2, 'flowerbed_A', 'garden',
     SDO_GEOMETRY(
     2003, 
@@ -35,7 +53,7 @@ INSERT INTO map VALUES (
   )
 );
 
-INSERT INTO map VALUES (
+INSERT INTO SEntities VALUES (
     3, 'flowerbed_B', 'garden',
     SDO_GEOMETRY(
     2003, 
@@ -48,7 +66,7 @@ INSERT INTO map VALUES (
   )
 );
 
-INSERT INTO map VALUES (
+INSERT INTO SEntities VALUES (
     4, 'fountain', 'poi',
     SDO_GEOMETRY(
     2003, 
@@ -62,7 +80,7 @@ INSERT INTO map VALUES (
   )
 );
 
-INSERT INTO map VALUES (
+INSERT INTO SEntities VALUES (
     5, 'central_road', 'road',
     SDO_GEOMETRY(
     2003, 
@@ -74,7 +92,7 @@ INSERT INTO map VALUES (
 );
 
 
-INSERT INTO map VALUES (
+INSERT INTO SEntities VALUES (
     6, 'aquarium', 'building',
     SDO_GEOMETRY(
     2003,
@@ -85,7 +103,7 @@ INSERT INTO map VALUES (
   )
 );
 
-INSERT INTO map VALUES (
+INSERT INTO SEntities VALUES (
     7, 'river', 'water',
     SDO_GEOMETRY(
     2003, 
@@ -106,7 +124,7 @@ INSERT INTO map VALUES (
   )
 );
 
-INSERT INTO map VALUES (
+INSERT INTO SEntities VALUES (
     8, 'bridge_north', 'building',
     SDO_GEOMETRY(
     2003,
@@ -120,7 +138,7 @@ INSERT INTO map VALUES (
 );
 
 
-INSERT INTO map VALUES (
+INSERT INTO SEntities VALUES (
     9, 'bridge_south', 'building',
     SDO_GEOMETRY(
     2003, 
@@ -133,7 +151,7 @@ INSERT INTO map VALUES (
   )
 );
 
-INSERT INTO map VALUES (
+INSERT INTO SEntities VALUES (
     10, 'staff_office', 'building',
     SDO_GEOMETRY(
     2003, NULL, NULL,
@@ -142,7 +160,7 @@ INSERT INTO map VALUES (
   )
 );
 
-INSERT INTO map VALUES (
+INSERT INTO SEntities VALUES (
     11, 'L_building', 'building',
     SDO_GEOMETRY(
     2003, NULL, NULL,
@@ -159,27 +177,12 @@ INSERT INTO map VALUES (
   )
 );
 
-INSERT INTO map VALUES (
-  12, 'zoo', 'border',
-  SDO_GEOMETRY(
-    2002,
-    NULL,
-    NULL,
-    SDO_ELEM_INFO_ARRAY(1,2,1),
-    SDO_ORDINATE_ARRAY(
-      0,0,
-      500,0,
-      500,500,
-      0,500,
-      0,0
-    )
-  )
-);
 
-DELETE FROM map
+
+DELETE FROM SEntities
 WHERE LOWER(entity_Name) = LOWER('bridge_north');
 
-UPDATE map
+UPDATE SEntities
 SET geometry = SDO_GEOMETRY(
     2003, NULL, NULL,
     SDO_ELEM_INFO_ARRAY(1,1003,3),
@@ -188,19 +191,99 @@ SET geometry = SDO_GEOMETRY(
 WHERE entity_Name = 'flowerbed_A';
 
 SELECT entity_Name, entity_Type
-FROM map
+FROM SEntities
 WHERE LOWER(entity_Type) = LOWER('building');
 
-SELECT entity_Id, entity_Name, entity_Type, geometry
-FROM map
+SELECT SID, entity_Name, entity_Type, geometry
+FROM SEntities
 WHERE LOWER(entity_Name) = LOWER('panda_House');
 
 SELECT entity_Name,
        SDO_GEOM.SDO_AREA(geometry, 0.005) AS area
-FROM map
+FROM SEntities
 WHERE SDO_GEOM.SDO_AREA(geometry, 0.005) > 10000;
 
-SELECT a.entity_Name FROM map a, map b
+SELECT a.entity_Name FROM SEntities a, SEntities b
 WHERE a.entity_Type = 'building'
   AND b.entity_Name = 'river'
   AND SDO_RELATE(a.geometry, b.geometry, 'mask=ANYINTERACT') = 'TRUE';
+
+
+
+CREATE TABLE MEntities (
+    MID        INTEGER PRIMARY KEY,
+    SID_ref  INTEGER REFERENCES SEntities(SID),
+    Title       VARCHAR2(100),
+    Image       ORDSYS.ORDImage,
+    Tokentime       DATE,
+    Image_SI ORDSYS.SI_StillImage,
+    Image_ac ORDSYS.SI_AverageColor,
+    Image_ch ORDSYS.SI_ColorHistogram,
+    Image_pc ORDSYS.SI_PositionalColor,
+    Image_tx ORDSYS.SI_Texture
+);
+
+CREATE OR REPLACE TRIGGER entities_generateFeatures
+  BEFORE INSERT OR UPDATE OF Image ON MEntities
+  FOR EACH ROW
+DECLARE
+  si ORDSYS.SI_StillImage;
+BEGIN
+
+  IF :NEW.Image.height IS NOT NULL THEN
+    BEGIN
+
+      si := new SI_StillImage(:NEW.Image.getContent());  
+
+
+      :NEW.Image_SI := si;
+      :NEW.Image_ac := SI_AverageColor(si);
+      :NEW.Image_ch := SI_ColorHistogram(si);
+      :NEW.Image_pc := SI_PositionalColor(si);
+      :NEW.Image_tx := SI_Texture(si);
+    EXCEPTION
+      WHEN OTHERS THEN
+
+        DBMS_OUTPUT.PUT_LINE('生成图像特征时发生错误: ' || SQLERRM);
+        RAISE; 
+    END;
+  END IF;
+END;
+
+/
+
+INSERT INTO MEntities (MID, SID_ref, Title, Image, Tokentime)
+VALUES (0,1,'with Rose',ORDSYS.ORDImage.init(),TO_DATE('2025-11-02 15:30:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO MEntities (MID, SID_ref, Title, Image, Tokentime) 
+VALUES (1, 1, 'beautiful', ordsys.ordimage.init(), TO_DATE('2025-11-02 18:00:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+
+INSERT INTO MEntities (MID, SID_ref, Title, Image, Tokentime) 
+VALUES (2, 1, 'beach', ordsys.ordimage.init(), TO_DATE('2025-11-02 21:30:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+
+UPDATE MEntities SET Tokentime = TO_DATE('2025-11-02 18:30:00', 'YYYY-MM-DD HH24:MI:SS') WHERE MID = 1;
+COMMIT;
+
+UPDATE MEntities SET SID_ref  = 0 WHERE MID = 1;
+COMMIT;
+
+UPDATE MEntities SET Title = 'Sunset Scene' WHERE MID = 1;
+COMMIT;
+
+UPDATE MEntities SET Title = 'With my best friend Rose' WHERE MID = 0;
+COMMIT;
+
+DELETE FROM MEntities WHERE MID = 0;
+COMMIT;
+
+SELECT image FROM MEntities WHERE SID_ref = 1;
+
+SELECT Image FROM MEntities WHERE Tokentime BETWEEN TO_DATE('2025-11-02 14:30:00', 'YYYY-MM-DD HH24:MI:SS') AND TO_DATE('2025-11-02 20:30:00', 'YYYY-MM-DD HH24:MI:SS');
+
+SELECT MID, Title, Tokentime, Image FROM MEntities WHERE LOWER(Title) LIKE LOWER('%sunset%');
+
+SELECT * FROM MEntities;
+
+
